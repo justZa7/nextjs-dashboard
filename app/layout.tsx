6cb114825ea1d6883b1cd8e9c31c7ac5612bebb0
justZa7 <mantha2116@gmail.com>
2024-07-30 10:01:31 +0700

    Pesan commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+import { Kodchasan } from 'next/font/google';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
